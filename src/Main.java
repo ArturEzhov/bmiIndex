0c41e1664ef38dfe98beac92d738424b50be4d25
Artur Ezhov <enproms@yandex.ru>
2023-04-09 21:34:59 +0500

    bmi Index

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+
+        double weightInKg = 88.5;
+        double heightInMeters = 1.76;
+
+        int bmi = service.calculate(weightInKg, heightInMeters);
+
+        System.out.println("Индекс массы тела: " + bmi);
     }
 }
